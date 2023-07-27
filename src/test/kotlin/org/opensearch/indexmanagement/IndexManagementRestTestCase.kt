d8a3ac65818d5a06266bf53b19f49a595a9545d4
Hailong Cui <ihailong@amazon.com>
2023-07-28 01:21:39 +0800

    Fix core XcontentType refactor (#873)
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    (cherry picked from commit d4cafd601c7c568af0b384f9b2e0da917408db64)

--- src/test/kotlin/org/opensearch/indexmanagement/IndexManagementRestTestCase.kt
+++ src/test/kotlin/org/opensearch/indexmanagement/IndexManagementRestTestCase.kt
@@ -24,7 +24,8 @@ import org.opensearch.core.xcontent.DeprecationHandler
 import org.opensearch.core.xcontent.NamedXContentRegistry
 import org.opensearch.common.xcontent.XContentType
 import org.opensearch.indexmanagement.indexstatemanagement.util.INDEX_HIDDEN
-import org.opensearch.rest.RestStatus
+import org.opensearch.core.rest.RestStatus
+import org.opensearch.core.xcontent.MediaType
 import java.io.IOException
 import java.nio.file.Files
 import java.util.*
@@ -194,7 +195,11 @@ abstract class IndexManagementRestTestCase : ODFERestTestCase() {
             }
 
             val response = client.performRequest(Request("GET", "/_cat/indices?format=json&expand_wildcards=all"))
+<<<<<<< HEAD
             val xContentType = XContentType.fromMediaType(response.entity.contentType.value)
+=======
+            val xContentType = MediaType.fromMediaType(response.entity.contentType)
+>>>>>>> d4cafd60 (Fix core XcontentType refactor (#873))
             xContentType.xContent().createParser(
                 NamedXContentRegistry.EMPTY, DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
                 response.entity.content
@@ -263,7 +268,11 @@ abstract class IndexManagementRestTestCase : ODFERestTestCase() {
         protected fun waitForThreadPools(client: RestClient) {
             val response = client.performRequest(Request("GET", "/_cat/thread_pool?format=json"))
 
+<<<<<<< HEAD
             val xContentType = XContentType.fromMediaType(response.entity.contentType.value)
+=======
+            val xContentType = MediaType.fromMediaType(response.entity.contentType)
+>>>>>>> d4cafd60 (Fix core XcontentType refactor (#873))
             xContentType.xContent().createParser(
                 NamedXContentRegistry.EMPTY, DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
                 response.entity.content
